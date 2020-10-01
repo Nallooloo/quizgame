582e15a5f3b85596e265daf7c489290814621b4f
unknown <mikael.maga@gmail.com>
2020-10-01 23:22:50 +0200

    added placeholder statspage

--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import PlayGame from "./PlayGame";
+import Stats from "./Stats";
 import Menu from "./Menu";
 
 const Game = () => {
@@ -9,6 +10,10 @@ const Game = () => {
       <Switch>
         <Route path="/play">
           <PlayGame />
+        </Route>
+
+        <Route path="/stats">
+          <Stats />
         </Route>
         <Route path="/">
           <Menu />
