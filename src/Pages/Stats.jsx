e4adc20dc850977c7288b2f0d807c1252756056d
unknown <mikael.maga@gmail.com>
2020-10-01 23:23:00 +0200

    placeholder statspage

---
+++ src/Pages/Stats.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Stats = () => {
+  return (
+    <div>
+      <div>some stats...</div>
+      <Link to="/play">Play agian!</Link>
+    </div>
+  );
+};
+
+export default Stats;
