5844bbfa769664d1ecb80b498863890849cd6995
unknown <mikael.maga@gmail.com>
2020-10-01 21:23:50 +0200

    added game

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import Game from "./Pages/Game";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
 }
 
 export default App;
