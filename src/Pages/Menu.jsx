5b4e2e169a9fb9685e4420080ad51c5175645a19
unknown <mikael.maga@gmail.com>
2020-10-01 21:24:26 +0200

    menu setup

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PlayGame = () => {
-  return <></>;
+  return (
+    <div>
+      <Link to="/play">New game</Link>
+    </div>
+  );
 };
 
 export default PlayGame;
