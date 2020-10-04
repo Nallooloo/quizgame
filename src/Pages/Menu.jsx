bb0e7347d8f7f726ebde32b774b8a464a3778d8f
unknown <mikael.maga@gmail.com>
2020-10-04 22:29:47 +0200

    copy pasta left some wrong names

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const PlayGame = () => {
+const Menu = () => {
   return (
     <div>
       <Link to="/play">New game</Link>
@@ -9,4 +9,4 @@ const PlayGame = () => {
   );
 };
 
-export default PlayGame;
+export default Menu;
