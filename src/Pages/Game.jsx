565bb60d62a0e378d59d118e2d0d374de7a93b0e
unknown <mikael.maga@gmail.com>
2020-10-04 22:30:59 +0200

    game now gets info on ans

--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import PlayGame from "./PlayGame";
 import Stats from "./Stats";
 import Menu from "./Menu";
 
 const Game = () => {
+  const [nbrCorrect, setNbrCorrect] = useState(0);
+  const nbrQuestions = 4;
+
+  const addOneCorrect = () => {
+    setNbrCorrect(nbrCorrect + 1);
+  };
+
+  const resetGame = () => {
+    setNbrCorrect(0);
+  };
+
   return (
     <Router>
       <Switch>
         <Route path="/play">
-          <PlayGame />
+          <PlayGame nbrQuestions={nbrQuestions} addOneCorrect={addOneCorrect} />
         </Route>
 
         <Route path="/stats">
-          <Stats />
+          <Stats
+            nbrCorrect={nbrCorrect}
+            nbrQuestions={nbrQuestions}
+            resetGame={resetGame}
+          />
         </Route>
         <Route path="/">
           <Menu />
