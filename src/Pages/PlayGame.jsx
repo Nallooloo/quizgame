141795ad86021a9d535371aaf77c3abac48a74be
unknown <mikael.maga@gmail.com>
2020-10-04 22:32:19 +0200

    moved state,added timer

--- src/Pages/PlayGame.jsx
+++ src/Pages/PlayGame.jsx
@@ -2,31 +2,42 @@ import React, { useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
 import Question from "../Components/Question";
 import SuperPower from "../Components/SuperPower";
+import CountDownTimer from "../Components/CountdownTimer";
 
-const someQuestion = [
+const someQuestions = [
   {
     question: "Who made this?",
     answers: ["Flydiverny", "MrFluFFy", "Abakar", "God"],
     correctAnswer: "MrFluFFy",
   },
+
   {
-    question: "Who made this yes?",
+    question: "Snus is probably made in...",
+    answers: ["Finland", "Norway", "Sweden", "Denmark"],
+    correctAnswer: "Sweden",
+  },
+  {
+    question: "Math is hard: 1+3",
+    answers: ["13", "31", "2", "4"],
+    correctAnswer: "4",
+  },
+  {
+    question: "Who is my brother?",
     answers: ["Flydiverny", "MrFluFFy", "Abakar", "God"],
-    correctAnswer: "MrFluFFy",
+    correctAnswer: "Flydiverny",
   },
 ];
 
-const PlayGame = () => {
-  const [answered, setAnswered] = useState("");
-  const [nbrCorrect, setNbrCorrect] = useState(0);
+const PlayGame = ({ nbrQuestions, addOneCorrect }) => {
   const [nbrAnswered, setNbrAnswered] = useState(0);
-  const [currQuestion, setCurrQuestion] = useState(someQuestion[0]);
+  const [currQuestion, setCurrQuestion] = useState(null);
   const [questionSet, setQuestionSet] = useState([]);
-  const nbrQuestions = 2;
 
   //"load the questions"
   useEffect(() => {
-    setQuestionSet(someQuestion);
+    setQuestionSet(someQuestions);
+    setCurrQuestion(someQuestions[0]);
+    console.log("i loaded questions");
   }, []);
 
   //on answer update question and rerender to display next question.
@@ -35,23 +46,25 @@ const PlayGame = () => {
   }, [questionSet, nbrAnswered]);
 
   const answerQuestion = (value) => {
-    setAnswered(value);
     if (value === currQuestion.correctAnswer) {
-      setNbrCorrect(nbrCorrect + 1);
+      addOneCorrect();
     }
 
     setNbrAnswered(nbrAnswered + 1);
   };
-
+  //move up stuff below to make it more clear what happens.
   return (
     <div>
       {nbrAnswered === nbrQuestions ? <Redirect to="/stats" /> : null}
       {currQuestion ? (
         <Question answerQuestion={answerQuestion} {...currQuestion} />
       ) : null}
+      <CountDownTimer />
       <SuperPower />
-      <div>Number correct: {nbrCorrect}</div>
-      <div>Number answered: {nbrAnswered}</div>
+
+      <div>
+        Number answered: {nbrAnswered}/{nbrQuestions}
+      </div>
     </div>
   );
 };
