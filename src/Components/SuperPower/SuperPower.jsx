3ffdc21cd21e9372a41c2243a38377a427212d28
unknown <mikael.maga@gmail.com>
2020-10-07 22:01:44 +0200

    minor styling, centering and a heading

--- src/Components/SuperPower/SuperPower.jsx
+++ src/Components/SuperPower/SuperPower.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import SuperCard from "../SuperCard/SuperCard";
+import styled from "styled-components";
+
+const SuperDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  width: 556px;
+  margin: auto;
+`;
+
+const Heading = styled.h4`
+  margin-bottom: 5px;
+  margin-top: 50px;
+`;
 const SuperPower = ({ superPowers = [], spendSuper, powersUsed = [] }) => {
   return (
     <div>
-      {superPowers.map((power) => {
-        return (
-          <SuperCard
-            name={power.name}
-            key={power.name}
-            spendSuper={spendSuper}
-            available={powersUsed.indexOf(power.name) === -1}
-          />
-        );
-      })}
+      <Heading>Use a super power if needed!</Heading>
+      <SuperDiv>
+        {superPowers.map((power) => {
+          return (
+            <SuperCard
+              name={power.name}
+              key={power.name}
+              spendSuper={spendSuper}
+              available={powersUsed.indexOf(power.name) === -1}
+            />
+          );
+        })}
+      </SuperDiv>
     </div>
   );
 };
