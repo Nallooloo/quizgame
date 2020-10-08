4df163f9822b3bb906b3f35358c20cb6c75b3689
unknown <mikael.maga@gmail.com>
2020-10-08 23:11:06 +0200

    displaying more stats

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Stats = ({ nbrCorrect, nbrQuestions, resetGame }) => {
+const Stats = ({
+  nbrCorrect,
+  nbrQuestions,
+  resetGame,
+  nbrMissed,
+  answerTimes,
+}) => {
   return (
     <div>
       <div>Number correct: {nbrCorrect}</div>
       <div>Number answered: {nbrQuestions}</div>
+      <div>Number missed: {nbrMissed}</div>
+      {answerTimes.map((time, index) => {
+        return <div key={index}>{time.toFixed(1)}</div>;
+      })}
       <Link to="/play" onClick={() => resetGame()}>
         Play agian!
       </Link>
