2b31c2343fc579344971dfd4093941471dd7d31a
unknown <mikael.maga@gmail.com>
2020-10-10 01:38:43 +0200

    some quick styling for newgame link

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NewGame = styled.div`
+  border-radius: 15px;
+  padding: 3px;
+  width: 250px;
+  height: 100px;
+  background: lightblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  margin-top: 25px;
+  border: 1px solid black;
+
+  &:hover {
+    background: #1db2ec;
+  }
+`;
+
+const NoUnderLineLink = styled(Link)`
+  text-decoration: none;
+`;
 
 const Menu = () => {
   return (
     <div>
-      <Link to="/play">New game</Link>
+      <NoUnderLineLink to="/play">
+        <NewGame>New game</NewGame>
+      </NoUnderLineLink>
     </div>
   );
 };
--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NewGame = styled.div`
+  border-radius: 15px;
+  padding: 3px;
+  width: 250px;
+  height: 100px;
+  background: lightblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  margin-top: 25px;
+  border: 1px solid black;
+
+  &:hover {
+    background: #1db2ec;
+  }
+`;
+
+const NoUnderLineLink = styled(Link)`
+  text-decoration: none;
+`;
 
 const Stats = ({
   nbrCorrect,
@@ -10,24 +33,51 @@ const Stats = ({
 }) => {
   return (
     <div>
-      <div>Number correct: {nbrCorrect}</div>
-      <div>Number answered: {nbrQuestions}</div>
-      <div>Number missed: {nbrMissed}</div>
-      <div>Fastest answer: {Math.min(...answerTimes).toFixed(2)}s</div>
-      <div>Slowest answer: {Math.max(...answerTimes).toFixed(2)}s</div>
       <div>
-        Average:
-        {answerTimes.reduce((acc, x) => {
-          return acc + x;
-        }) / answerTimes.length}
-        s
+        <h4>Questions</h4>
+        <div>Number correct: {nbrCorrect}</div>
+        <div>Number answered: {nbrQuestions}</div>
+        <div>Number missed: {nbrMissed}</div>
       </div>
-      {answerTimes.map((time, index) => {
-        return <div key={index}>{time.toFixed(2)}</div>;
-      })}
-      <Link to="/play" onClick={() => resetGame()}>
-        Play agian!
-      </Link>
+
+      <div>
+        <h4>Speed!</h4>
+        <div>
+          Fastest answer:
+          {answerTimes.length > 0 ? (
+            <>{Math.min(...answerTimes).toFixed(2)}s </>
+          ) : (
+            <>not available</>
+          )}
+        </div>
+        <div>
+          Slowest answer:
+          {answerTimes.length > 0 ? (
+            <>{Math.max(...answerTimes).toFixed(2)}s </>
+          ) : (
+            <>not available</>
+          )}
+        </div>
+        <div>
+          Average:
+          {answerTimes.length > 0 ? (
+            <>
+              {(
+                answerTimes.reduce((acc, x) => {
+                  return acc + x;
+                }) / answerTimes.length
+              ).toFixed(2)}
+              s
+            </>
+          ) : (
+            <>not available</>
+          )}
+        </div>
+      </div>
+
+      <NoUnderLineLink to="/play" onClick={() => resetGame()}>
+        <NewGame>Play agian!</NewGame>
+      </NoUnderLineLink>
     </div>
   );
 };
