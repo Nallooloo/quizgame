0601d74b66878562623b6e13d0faadc8db055ff3
unknown <mikael.maga@gmail.com>
2020-10-10 01:07:57 +0200

    some more stats

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -13,6 +13,15 @@ const Stats = ({
       <div>Number correct: {nbrCorrect}</div>
       <div>Number answered: {nbrQuestions}</div>
       <div>Number missed: {nbrMissed}</div>
+      <div>Fastest answer: {Math.min(...answerTimes).toFixed(2)}s</div>
+      <div>Slowest answer: {Math.max(...answerTimes).toFixed(2)}s</div>
+      <div>
+        Average:
+        {answerTimes.reduce((acc, x) => {
+          return acc + x;
+        }) / answerTimes.length}
+        s
+      </div>
       {answerTimes.map((time, index) => {
         return <div key={index}>{time.toFixed(2)}</div>;
       })}
